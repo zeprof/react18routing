75d4dfeaabad1a7ee618ed5bd8f7111973df3a1f
Francois Lacoursiere <lacouf@gmail.com>
2022-04-29 11:20:37 -0400

    Demo de l'utilisation de Routing avec React 18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./components/HomePage";
+import About from "./components/About";
+import Header from "./components/Header";
 import './App.css';
+import PageNotFound from "./components/PageNotFound";
+import Bibliotheque from "./components/Bibliotheque";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div >
+            <header >
+                <h1>Header Demo Router</h1>
+                <Header/>
+            </header>
+
+            <Routes>
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/bibliotheque" element={<Bibliotheque/>} />
+                <Route path="*" element={<PageNotFound/>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
